d2f7df459f889f4609aa0682f841c0483e8d22f9
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    polish StringExtensionsTestsJsonExtensions

--- tests/dotnet-workflow-engine.Tests/StringExtensionsTestsJsonExtensions.cs
+++ tests/dotnet-workflow-engine.Tests/StringExtensionsTestsJsonExtensions.cs
@@ -21,7 +21,7 @@ using System.Text.Json.Serialization;
 namespace DotNetWorkflowEngine.Tests;
 
 /// <summary>
-/// Provides JSON serialization and deserialization extension methods for the <see cref="StringExtensionsTests"/> class.
+/// Provides JSON serialization and deserialization extension methods for strings.
 /// </summary>
 public static class StringExtensionsJsonExtensions
 {
@@ -33,13 +33,14 @@ public static class StringExtensionsJsonExtensions
     };
 
     /// <summary>
-    /// Serializes the <see cref="StringExtensionsTests"/> instance to a JSON string.
+    /// Serializes the specified object to a JSON string.
     /// </summary>
+    /// <typeparam name="T">The type of the object to serialize.</typeparam>
     /// <param name="value">The instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <returns>A JSON string representation of the instance.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this StringExtensionsTests value, bool indented = false)
+    public static string ToJson<T>(this T value, bool indented = false) where T : notnull
     {
         ArgumentNullException.ThrowIfNull(value);
 
@@ -54,32 +55,31 @@ public static class StringExtensionsJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a <see cref="StringExtensionsTests"/> instance.
+    /// Deserializes a JSON string to an instance of type <typeparamref name="T"/>.
     /// </summary>
+    /// <typeparam name="T">The type to deserialize into.</typeparam>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized instance, or null if the JSON is null or whitespace.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static StringExtensionsTests? FromJson(string json)
+    public static T? FromJson<T>(string json) where T : class
     {
         ArgumentNullException.ThrowIfNull(json);
 
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
-
-        return JsonSerializer.Deserialize<StringExtensionsTests>(json, _jsonSerializerOptions);
+        return string.IsNullOrWhiteSpace(json)
+            ? null
+            : JsonSerializer.Deserialize<T>(json, _jsonSerializerOptions);
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="StringExtensionsTests"/> instance.
+    /// Attempts to deserialize a JSON string to an instance of type <typeparamref name="T"/>.
     /// </summary>
+    /// <typeparam name="T">The type to deserialize into.</typeparam>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized instance if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    public static bool TryFromJson(string json, out StringExtensionsTests? value)
+    public static bool TryFromJson<T>(string json, out T? value) where T : class
     {
         value = null;
 
@@ -92,8 +92,8 @@ public static class StringExtensionsJsonExtensions
 
         try
         {
-            value = JsonSerializer.Deserialize<StringExtensionsTests>(json, _jsonSerializerOptions);
-            return true;
+            value = JsonSerializer.Deserialize<T>(json, _jsonSerializerOptions);
+            return value is not null;
         }
         catch (JsonException)
         {
