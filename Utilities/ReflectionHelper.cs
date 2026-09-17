9a7964d3aa227ffd88ec8c0b1211dec4511c1ddf
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-17 00:52:52 +0000

    chore: maintenance updates

--- Utilities/ReflectionHelper.cs
+++ Utilities/ReflectionHelper.cs
@@ -258,6 +258,16 @@ public static class ReflectionHelper
 /// </summary>
 public class PropertyAccessException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PropertyAccessException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public PropertyAccessException(string message) : base(message) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PropertyAccessException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
     public PropertyAccessException(string message, Exception innerException) : base(message, innerException) { }
 }
